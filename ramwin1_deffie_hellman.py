be38a539769bdab5809dfd9fa8a956cc51b2a0f3
Xiang Wang <1846468966@qq.com>
2015-12-01 11:03:51 +0800

    把后台的python脚本也放进去了

--- ramwin1_deffie_hellman.py
+++ ramwin1_deffie_hellman.py
@@ -10,21 +10,11 @@ def power(y,d,n):
         return y%n
     return (y**d)%n
 def main():
-    p = 82793
-    g = 32767
-    a = 1541
-    b = 1000;
-    # ga = power(g,a,p)
-    # b = 55555
-    # message = 24929
-    # gab = power(ga,b,p)
-    # gb = power(g,b,p)
-    # crypto_text = message^gab
-    # gba = power(gb,a,p)
-    # decrypto_text = crypto_text^gba
-    # gba = power(gb,a,p)
-    # decrypto_text = crypto_text^gba
-    gb = 52044
+    p = 421542432643
+    g = 542432643
+    a = 248123464587
+    b = 192458139446
+    gb = 248183040540
     ga = power(g,a,p)
     print("ga={0}".format(ga))
     gb = power(g,b,p)
