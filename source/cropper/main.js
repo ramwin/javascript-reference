c26e78d167f6bec1e3c9cfaf94377f86c8e8daa6
Xiang Wang <ramwin@qq.com>
2016-09-06 20:19:09 +0800

    截取头像

---
+++ source/cropper/main.js
@@ -0,0 +1,51 @@
+$(function () {
+
+  'use strict';
+
+  var console = window.console || { log: function () {} };
+  var $image = $('#image');
+  var options = {
+        aspectRatio: 16 / 9,
+        preview: '.img-preview',
+        crop: function (e) {
+        }
+      };
+
+  // Cropper
+  $image.on({}).cropper(options);
+
+  // Import image
+  var $inputImage = $('#inputImage');
+  var URL = window.URL || window.webkitURL;
+  var blobURL;
+
+  if (URL) {
+    $inputImage.change(function () {
+      var files = this.files;
+      var file;
+
+      if (!$image.data('cropper')) {
+        return;
+      }
+
+      if (files && files.length) {
+        file = files[0];
+
+        if (/^image\/\w+$/.test(file.type)) {
+          blobURL = URL.createObjectURL(file);
+          $image.one('built.cropper', function () {
+
+            // Revoke when load complete
+            URL.revokeObjectURL(blobURL);
+          }).cropper('reset').cropper('replace', blobURL);
+          $inputImage.val('');
+        } else {
+          window.alert('Please choose an image file.');
+        }
+      }
+    });
+  } else {
+    $inputImage.prop('disabled', true).parent().addClass('disabled');
+  }
+
+});
