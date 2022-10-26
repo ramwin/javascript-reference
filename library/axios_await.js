2cf9df528efb68bd40fc33386beefdc1975ed048
Xiang Wang <ramwin@qq.com>
2022-10-26 20:48:23 +0800

    await

--- library/axios_await.js
+++ library/axios_await.js
@@ -10,29 +10,38 @@ function get_data() {
   })
 }
 
-function async_get_data() {
-  console.info("调用async_get_data")
-  get_data()
-  console.info("调用结束")
-}
-
 function sync_get_data() {
-  get_data().then(res=>{
-    console.info("调用结束")
-    console.info(res)
+  return get_data().then(res=>{  // 第一个return代表返回的是Promise
+    console.info("调用结束", res)
+    return res;  // 第二个return代表返回了什么数据在then
   })
 }
 
-async function await_data() {
-  var result = await get_data();
-  console.info("await结束")
-  console.info(result)
+function test1() {
+  sync_get_data().then(res=>{
+    console.info("sync_get_data")
+    console.info(res)
+  })
+};
+
+async function async_get_data() {
+  console.info("调用async_get_data")
+  get_data()  // 存在return才能被await
+  console.info("async_get_data 调用结束")
+}
+
+function test2() {
+  console.info("调用async的函数, 直接调用. 先返回了")
+  async_get_data()
 }
 
 
-function test() {
-  // await await_data();  // 这个会报错
-  console.info("end")
-};
+async function test3() {
+  console.info("调用async的函数, 直接调用. 先返回了")
+  // await sync_get_data()  // 返回的是Promise, 会等待
+  await async_get_data()
+  console.info("返回的不是Promise, 直接不等待")
+  console.info("只有async的函数, 可以await")
+}
 
-test();
+test3()
