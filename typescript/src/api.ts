4497c34bc1a6fe837c99359cdce23e471c14dbee
Xiang Wang <ramwin@qq.com>
2023-08-26 22:45:49 +0800

    feat: request接收参数

--- typescript/src/api.ts
+++ typescript/src/api.ts
@@ -1,35 +1,38 @@
 import axios from "axios";
 
-
 function test1() {
-    axios.get(
-        "https://httpbin.org/json",
-    ).then((res)=>{
-        console.log(res.data);
-    })
+  axios.get("https://httpbin.org/json").then((res) => {
+    console.log(res.data);
+  });
 }
 
 function test2() {
-    // 利用结构减少代码
-    axios.get(
-        "https://httpbin.org/json",
-    ).then(({data})=>{
-        console.log(data);
-    })
+  // 利用结构减少代码
+  axios.get("https://httpbin.org/json").then(({ data }) => {
+    console.log(data);
+  });
 }
 
 function test3() {
-    // 利用interface代表接口
-    interface Data {
-        author: string
-    }
-    let obj: Data = {"author": ""}
-    axios.get(
-        "https://httpbin.org/json",
-    ).then(({data})=>{
-        obj = data; 
-        console.log(obj);
-    })
+  // 利用interface代表接口
+  interface show {
+    author: string;
+    date: string;
+  }
+  interface Data {
+    slideshow: show;
+  }
+  let obj: show = {
+    author: "",
+    date: "",
+  };
+  axios
+    .get<any, { data: Data }>("https://httpbin.org/json")
+    .then(({ data }) => {
+      console.log("返回结果: ", data);
+      obj = data.slideshow;
+      console.log(obj);
+    });
 }
 
-test3()
+test3();
