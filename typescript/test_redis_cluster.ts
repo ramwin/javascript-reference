8b14ff50ad514e48ecf87610e75ab25b353d4dad
Xiang Wang <ramwin@qq.com>
2025-05-05 23:58:42 +0800

    fix: 连不上就报错

--- typescript/test_redis_cluster.ts
+++ typescript/test_redis_cluster.ts
@@ -3,16 +3,30 @@ import { createCluster } from "redis";
 
 async function main() {
   console.log("测试连接集群")
-  const cluster = await createCluster({
+  const cluster = createCluster({
       rootNodes: [
           {url: "redis://127.0.0.1:7000"},
           {url: "redis://127.0.0.1:7001"},
       ]
-  }).connect()
+  })
+  const client = await cluster.connect()
   console.log("一连接", cluster)
-  console.log(await cluster.get("1"));
+  console.log(await client.get("1"));
   console.log("结束")
   cluster.close()
 }
 
-main()
+async function test_connect_fail() {
+  console.log("测试连接集群")
+  const cluster = createCluster({
+      rootNodes: [
+          {url: "redis://127.0.0.1:7008"},
+      ]
+  })
+  // 连不上会报错
+  const client = await cluster.connect()
+  console.log(await client.get("foo"))
+  client.close()
+}
+
+test_connect_fail()
