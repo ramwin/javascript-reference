8dc3b8f37626e8211168bbeecddc705c915566f3
Xiang Wang <ramwin@qq.com>
2025-06-01 23:20:10 +0800

    fix: test log

--- typescript/test_log.ts
+++ typescript/test_log.ts
@@ -7,3 +7,5 @@ const log = new Logger({
 });
 
 log.info('This is a log message'); // will output file name and line number
+log.warn('This is a warning message'); // will output file name and line number
+log.error('This is a error message'); // will output file name and line number
